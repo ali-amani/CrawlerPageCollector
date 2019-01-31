110d41e9a76c0b661c7dd4211ff513527ea17d6d
Ali Amani <a.amani1073@gmail.com>
2019-01-31 17:11:21 +0330

    export excell

--- src/main/java/Business/EnginePageCollector.java
+++ src/main/java/Business/EnginePageCollector.java
@@ -6,6 +6,10 @@ import Models.ShopPageContent;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 import org.jsoup.Jsoup;
@@ -13,6 +17,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,10 +32,39 @@ public class EnginePageCollector {
 
     private List<Pivot> pivotList;
     private Map<String, List<PageContent>> mapOfMarkets;
-    private MongoClient mongoClient;
+    private com.mongodb.client.MongoClient mongoClient;
+    private int rownum ;
+    private static final String FILE_NAME1 = "./رستوران و کافی شاپ.xlsx";
+    private static final String FILE_NAME2 = "./هنر و تئاتر.xlsx";
+    private static final String FILE_NAME3 = "./تفریحی و ورزشی.xlsx";
+    private static final String FILE_NAME4 = "./آموزش.xlsx";
+    private static final String FILE_NAME5 = "./سلامتی و پزشکی.xlsx";
+    private static final String FILE_NAME6 = "./زیبایی و آرایش.xlsx";
+    private static final String FILE_NAME7 = "./تور و سفر.xlsx";
+    private static final String FILE_NAME = "./اطلاعات تخفیفان.xlsx";
+    private Object[] columnNames = {"نام", "لینک" , "شرایط استفاده" , "میزان تخفیف" ,
+            "قیمت" , "لیست تصاویر" , "توضیحات" , "ویژگی ها" ,  "اطلاعات" , "مختصات"};
+
+    private XSSFWorkbook workbook ;
+    private XSSFSheet sheet ;
 
     public EnginePageCollector(List<Pivot> pivotList) {
         this.pivotList = pivotList;
+        this.rownum = 0 ;
+        this.workbook = new XSSFWorkbook();
+        this.sheet = workbook.createSheet("takhfifan datas");
+        Row row = sheet.createRow(rownum++);
+        int colNum = 0 ;
+        for (Object object:columnNames){
+            Cell cell = row.createCell(colNum++);
+
+            if (object instanceof String) {
+                cell.setCellValue((String) object);
+            } else if (object instanceof Integer) {
+                cell.setCellValue((Integer) object);
+            }
+        }
+
         mapOfMarkets = new HashMap<String, List<PageContent>>();
 
         CodecRegistry pojoCodecRegistry = fromRegistries(com.mongodb.MongoClient.getDefaultCodecRegistry(),
@@ -49,15 +85,42 @@ public class EnginePageCollector {
 
             for (Pivot pivot : pivotList) {
                 Document doc = Jsoup.connect(pivot.getPivot()).get();
-                System.out.println("parent link : " + doc.title());
-                MongoCollection<PageContent> collection = database.getCollection(doc.title(), PageContent.class);
-                if (!collectionNames.into(new ArrayList<String>()).contains(doc.title()))
-                    database.createCollection(doc.title());
+                String title = doc.title() ;
+//                if(title.length()>100)
+                title = title.substring(0,35);
+                System.out.println("parent link : " + title);
+                MongoCollection<PageContent> collection = database.getCollection(title, PageContent.class);
+                if (!collectionNames.into(new ArrayList<String>()).contains(title))
+                    database.createCollection(title);
+
+                Row row = sheet.createRow(rownum++);
+                Cell cell = row.createCell(0);
+                cell.setCellValue((String) title);
 
                 List<PageContent> pageContents = getShopInfo(doc);
                 collection.insertMany(pageContents);
-                this.mapOfMarkets.put(doc.title(), pageContents);
+                this.mapOfMarkets.put(title, pageContents);
                 System.out.println("/////////////////////////////////");
+            }
+            try {
+                sheet.autoSizeColumn(0);
+                sheet.autoSizeColumn(1);
+                sheet.autoSizeColumn(2);
+                sheet.autoSizeColumn(3);
+                sheet.autoSizeColumn(4);
+
+                sheet.autoSizeColumn(6);
+                sheet.autoSizeColumn(7);
+                sheet.autoSizeColumn(8);
+                sheet.autoSizeColumn(9);
+
+                FileOutputStream outputStream = new FileOutputStream(FILE_NAME7);
+                workbook.write(outputStream);
+                workbook.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
         } catch (Exception e) {
@@ -73,25 +136,62 @@ public class EnginePageCollector {
         for (Element element : elements) {
             String name = doc.select(".deal-title").get(count).text();
             String link = element.absUrl("href");
-            System.out.println(count + " >> name of market : " + name + "link : " + link);
-            links.add(new PageContent(link, name, getShopContent(link)));
-            count++;
-            if (count == 10)
-                break;
+            count ++ ;
+//            if(count-1 == 180 || count-1 == 90 )
+//                continue;
+            System.out.println(count-1 + " >> name of market : " + name + "link : " + link);
+            ShopPageContent shopContent = getShopContent(link) ;
+            //filling excel
+            int colNum = 0 ;
+            Row row = sheet.createRow(rownum++);
+
+            Cell cell1 = row.createCell(colNum++);
+            cell1.setCellValue((String) name);
+            Cell cell2 = row.createCell(colNum++);
+            cell2.setCellValue((String) link);
+            Cell cell3 = row.createCell(colNum++);
+            cell3.setCellValue((String) shopContent.getUseConditions().toString());
+            Cell cell4 = row.createCell(colNum++);
+            cell4.setCellValue((String) shopContent.getDiscount());
+            Cell cell5 = row.createCell(colNum++);
+            cell5.setCellValue((String) shopContent.getPrices().toString());
+            Cell cell6 = row.createCell(colNum++);
+            cell6.setCellValue((String) shopContent.getGallery().toString());
+            Cell cell7 = row.createCell(colNum++);
+            cell7.setCellValue((String) shopContent.getDescription().toString());
+            Cell cell8 = row.createCell(colNum++);
+            cell8.setCellValue((String) shopContent.getFeatures().toString());
+            Cell cell9 = row.createCell(colNum++);
+            cell9.setCellValue((String) shopContent.getShopInfo().toString());
+            Cell cell10 = row.createCell(colNum++);
+            cell10.setCellValue((String) shopContent.getMapCoordinate());
+
+
+
+            System.out.println("Done");
+            links.add(new PageContent(link, name, shopContent));
+
         }
+
         return links;
     }
 
     private ShopPageContent getShopContent(String link) {
         ShopPageContent shopPageContent = new ShopPageContent();
         Document doc;
+
         try {
 
             doc = Jsoup.connect(link).get();
+
             List<String> useConditions = doc.select(".deal-custom-conditions .list li").eachText(); // useConditions
+
             String name = doc.select(".vendor-name span").text(); // Vendor Name
+
             String discount = doc.select(".deal-discount-number").first().text(); // Discount
-            List<String> prices = doc.select(".deal-details-right-section").select(".deal-price").eachText();
+
+            List<String> prices = doc.select(".deal-details-right-section")
+                    .select(".deal-price").eachText();
             //images
             List<String> Gallery = doc.select(".slide_element div img").eachAttr("data-src");
             //Description
--- src/main/java/WebCrawler.java
+++ src/main/java/WebCrawler.java
@@ -1,26 +1,83 @@
 import Business.EnginePageCollector;
 import Models.Pivot;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//import com.sun.rowset.internal.Row;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class WebCrawler {
+
+    private static final String FILE_NAME = "./MyFirstExcel.xlsx";
+
     public static void main(String[] args) {
         // TODO code application logic here
+
+
+//        XSSFWorkbook workbook = new XSSFWorkbook();
+//        XSSFSheet sheet = workbook.createSheet("Takhfifan Datas");
+        /*Object[][] datatypes = {
+                {"Datatype", "Type", "Size(in bytes)"},
+                {"int", "Primitive", 2},
+                {"float", "Primitive", 4},
+                {"double", "Primitive", 8},
+                {"char", "Primitive", 1},
+                {"String", "Non-Primitive", "No fixed size"}
+        };*/
+
+        int rowNum = 0;
+        System.out.println("Creating excel");
+
+        /*
+        *
+        * for (Object[] datatype : datatypes) {
+            Row row = sheet.createRow(rowNum++);
+            int colNum = 0;
+            for (Object field : datatype) {
+                Cell cell = row.createCell(colNum++);
+                if (field instanceof String) {
+                    cell.setCellValue((String) field);
+                } else if (field instanceof Integer) {
+                    cell.setCellValue((Integer) field);
+                }
+            }
+        }*/
+
+//        try {
+//            FileOutputStream outputStream = new FileOutputStream(FILE_NAME);
+//            workbook.write(outputStream);
+//            workbook.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        System.out.println("Done");
+
         List<Pivot> pvs = new ArrayList<Pivot>();
         //pvs.add(new Pivot("https://github.com/yasserg/crawler4j"));
-        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
-                + "%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86-%D9%88-%DA%A9%D8%A7%D9%81%DB%8C-%D8%B4%D8%A7%D9%BE"));
-        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
-                + "%D8%AA%D8%A6%D8%A7%D8%AA%D8%B1-%D9%88-%D9%87%D9%86%D8%B1"));
-        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
-                + "%D8%AA%D9%81%D8%B1%DB%8C%D8%AD%DB%8C-%D9%88-%D9%88%D8%B1%D8%B2%D8%B4%DB%8C"));
-        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
-                + "%D8%A2%D9%85%D9%88%D8%B2%D8%B4%DB%8C"));
-        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
-                + "%D8%B3%D9%84%D8%A7%D9%85%D8%AA%DB%8C-%D9%88-%D9%BE%D8%B2%D8%B4%DA%A9%DB%8C"));
-        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
-                + "%D8%B2%DB%8C%D8%A8%D8%A7%DB%8C%DB%8C-%D9%88-%D8%A2%D8%B1%D8%A7%DB%8C%D8%B4%DB%8C"));
+//        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
+//                + "%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86-%D9%88-%DA%A9%D8%A7%D9%81%DB%8C-%D8%B4%D8%A7%D9%BE"));
+//        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
+//                + "%D8%AA%D8%A6%D8%A7%D8%AA%D8%B1-%D9%88-%D9%87%D9%86%D8%B1"));
+//        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
+//                + "%D8%AA%D9%81%D8%B1%DB%8C%D8%AD%DB%8C-%D9%88-%D9%88%D8%B1%D8%B2%D8%B4%DB%8C"));
+//        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
+//                + "%D8%A2%D9%85%D9%88%D8%B2%D8%B4%DB%8C"));
+//        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
+//                + "%D8%B3%D9%84%D8%A7%D9%85%D8%AA%DB%8C-%D9%88-%D9%BE%D8%B2%D8%B4%DA%A9%DB%8C"));
+//        pvs.add(new Pivot("https://takhfifan.com/cities/%D8%AA%D9%87%D8%B1%D8%A7%D9%86/"
+//                + "%D8%B2%DB%8C%D8%A8%D8%A7%DB%8C%DB%8C-%D9%88-%D8%A2%D8%B1%D8%A7%DB%8C%D8%B4%DB%8C"));
         pvs.add(new Pivot("https://takhfifan.com/global/"
                 + "%D8%AA%D8%AE%D9%81%DB%8C%D9%81%D8%A7%D9%86%20%D9%87%D8%A7%DB%8C%20%D9%85%D8%B3%D8%A7%D9%81%D8%B1%D8%AA%DB%8C"));
         EnginePageCollector collector = new EnginePageCollector(pvs);
